b2fac0efc3bca5568ee2270ace4da546f3964d6f
Chab-Jose <alejandro.pumas13@gmail.com>
2025-04-23 21:57:09 -0600

    initial configuration

---
+++ resources/js/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "../js/react-component/page/login"
+
+export default function App() {
+    return (
+        <Login />
+    );
+}
+
+
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel con React</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.js','resources/js/main.jsx'])
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
